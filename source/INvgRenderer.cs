4f28d1bd59bf5853c2fac17f88cf4137d3aaef01
Klaus Bergius <50986374+kbergius@users.noreply.github.com>
2025-09-09 15:07:03 +0300

    INvgRenderer

--- source/INvgRenderer.cs
+++ source/INvgRenderer.cs
@@ -6,26 +6,46 @@ namespace NanoUI
 {
     public interface INvgRenderer
     {
+        /// <summary>
+		/// Creates a texture from the specified path with texture options (flags)
+		/// </summary>
         int CreateTexture(string path, TextureFlags textureFlags = 0);
 
-        // this is used for creating textures for images (RGBA) & font atlas (R)
+        /// <summary>
+		/// Creates a texture from the texture description
+		/// </summary>
         int CreateTexture(TextureDesc description);
+
+        /// <summary>
+		/// Updates a texture with data
+		/// </summary>
         bool UpdateTexture(int texture, ReadOnlySpan<byte> data);
 
         // The basic purpose here is to just resize texture. It doesn't necessarily
         // copy data from the old texture to the new one. You can of course do it, if you like.
         // note: NanoUI uses this function - when it wants to resize font atlas texture -
         // this way: it first calls ResizeTexture and then calls UpdateTexture with new data.
+        /// <summary>
+		/// Resizes a texture according to texture desctiption
+		/// </summary>
         bool ResizeTexture(int texture, TextureDesc description);
 
         // this is called also when font atlas texture is resized
         // (delete old, create new)
+        /// <summary>
+		/// Deletes a texture
+		/// </summary>
         bool DeleteTexture(int texture);
 
+        /// <summary>
+		/// Gets a texture size (width, height)
+		/// </summary>
         bool GetTextureSize(int texture, out Vector2 size);
 
-        // triggerred from NvgContext when EndFrame called
-        // note: NvgContext clears DrawCache after it has called this method
+        /// <summary>
+		/// Triggerred from NvgContext when EndFrame called.
+        /// DrawCache is cleared after this
+		/// </summary>
         void Render();
     }
 }
