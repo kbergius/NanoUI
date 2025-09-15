81f2d680af28296118766464baf45ddcfae865d2
Klaus Bergius <50986374+kbergius@users.noreply.github.com>
2025-09-15 14:16:44 +0300

    Globals

--- source/Globals.cs
+++ source/Globals.cs
@@ -3,103 +3,113 @@
 namespace NanoUI
 {
     /// <summary>
-    /// Globals.
+    /// Globals has global values for NanoUI.
     /// </summary>
     public class Globals
     {
-        // this is used to indicate that int value (that points to struct/class) is not defined/incorrect.
-        // used with textures, fonts, glyphs etc
-        // note: basically all negative values are treated as invalid
-
         /// <summary>
-        /// INVALID.
+        /// Used to indicate that methods that return int value is not defined/incorrect/missing.
+        /// Used mainly with textures, fonts, icons, glyphs. Value: -1.
+        /// Note: basically all negative values are treated as invalid.
         /// </summary>
         public const int INVALID = -1;
 
-        // Uncomment the following definition to draw red bounding
-        // boxes around widgets(useful for debugging drawing code)
-
         /// <summary>
-        /// DEBUG_WIDGET_BOUNDS.
+        /// Set to true, if you want to draw red bounding rectangles around widgets.
+        /// Default value: false.
         /// </summary>
         public static bool DEBUG_WIDGET_BOUNDS = false;
 
         /// <summary>
-        /// SHOW_TOOLTIPS.
+        /// Global setting to determine, if tooltips are shown.
+        /// Default value: true;
         /// </summary>
         public static bool SHOW_TOOLTIPS = true;
 
-        // these are initial font texture values. if texture becomes full, it informs user to expand texture
-
         /// <summary>
-        /// FONT_TEXTURE_WIDTH.
+        /// Initial font texture width. Default value: 1024.
+        /// If font texture becomes full, NanoUI informs user to expand it.
         /// </summary>
         public static int FONT_TEXTURE_WIDTH = 1024;
 
         /// <summary>
-        /// FONT_TEXTURE_HEIGHT.
+        /// Initial font texture height. Default value: 512.
+        /// If font texture becomes full, NanoUI informs user to expand it.
         /// </summary>
         public static int FONT_TEXTURE_HEIGHT = 512;
 
         /// <summary>
-        /// MAX_TEXT_ROWS.
+        /// Maximum text rows that NanoUI processes. Default value: 1024.
         /// </summary>
         public static int MAX_TEXT_ROWS = 1024;
 
-        // we restrict layout updates per frame (used in screen)
-
         /// <summary>
-        /// MAX_LAYOUT_UPDATE_PER_FRAME.
+        /// When calling RequestLayoutUpdate, NanoUI queues commands. 
+        /// This value tells, howe many queued commands are processed in scene Draw
+        /// method / frame. Value: 10.
         /// </summary>
         public const int MAX_LAYOUT_UPDATE_PER_FRAME = 10;
 
-        // this determines the time delay (when button is down) after repeated action is fired
-        // or when we should update some values (like performance graph text)
-        // value is in seconds
-
         /// <summary>
-        /// UPDATE_DELAY.
+        /// Determines the time delay (when button is down) after repeated action is fired
+        /// or when NanoUI should update some values in widgets.
+        //  Value is in seconds.
         /// </summary>
         public static float UPDATE_DELAY = 0.1f;
 
-        // shall we make caret in active textbox blink?
-
         /// <summary>
-        /// BLINK_TEXTBOX_CARET.
+        /// Make caret in active text field blink. Default = true.
         /// </summary>
         public static bool BLINK_TEXTBOX_CARET = true;
 
-        // this is used if folder path not specified or specified path is invalid
-
         /// <summary>
-        /// DEFAULT_FOLDER_PATH.
+        /// Used if folder path is not specified or specified path is invalid.
+        /// Default value: Environment.CurrentDirectory.
         /// </summary>
         public static string DEFAULT_FOLDER_PATH = Environment.CurrentDirectory;
 
-        // Files / folders
-
         /// <summary>
-        /// FILEFOLDER_DIRECTORY_UP.
+        /// File/folder widgets directory up string. Default value: "...".
         /// </summary>
         public static string FILEFOLDER_DIRECTORY_UP = "...";
 
-        // DEFAULT CATEGORIES
-        // These are attributes that all objects properties can have
-        // (System.ComponentModel.CategoryAttribute)
-        // note : these are used as ids in categorySorter by now
+        /// <summary>
+        /// Category BASIC to group many properties in widgets into same group.
+        /// </summary>
         public const string CATEGORY_BASIC = "Basic";
-        public const string CATEGORY_LAYOUT = "Layout";
-        public const string CATEGORY_APPEARANCE = "Appearance";
-        public const string CATEGORY_TEXT = "Text";
-        public const string CATEGORY_BEHAVIOR = "Behavior";
-        public const string CATEGORY_TRANSFORM = "Transform";
-        // no spesific category
-        public const string CATEGORY_MISC = "Misc";
-
-        // the order of this array is used as default category sort order in CategorySorter
 
         /// <summary>
-        /// GetDefaultCategoryIds.
+        /// Category LAYOUT to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_LAYOUT = "Layout";
+
+        /// <summary>
+        /// Category APPEARANCE to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_APPEARANCE = "Appearance";
+
+        /// <summary>
+        /// Category TEXT to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_TEXT = "Text";
+
+        /// <summary>
+        /// Category BEHAVIOR to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_BEHAVIOR = "Behavior";
+
+        /// <summary>
+        /// Category TRANSFORM to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_TRANSFORM = "Transform";
+        
+        /// <summary>
+        /// Category MISC (no spesific category) to group many properties in widgets into same group.
+        /// </summary>
+        public const string CATEGORY_MISC = "Misc";
+
+        /// <summary>
+        /// Order of the categories to display.
         /// </summary>
         public static string[] GetDefaultCategoryIds()
         {
