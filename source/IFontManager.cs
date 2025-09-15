c017fdbe64c5b021109b149fd14c63f317efcc45
Klaus Bergius <50986374+kbergius@users.noreply.github.com>
2025-09-15 14:48:25 +0300

    IFontManager

--- source/IFontManager.cs
+++ source/IFontManager.cs
@@ -6,77 +6,74 @@ using System;
 namespace NanoUI
 {
     /// <summary>
-    /// IFontManager.
+    /// IFontManager is an interface to font manager implementations.
+    /// NanoUI has 2 built-in font managers based on StbTrueType (safe & unsafe),
+    /// but you can also create your own preferred font manager.
+    /// Note: If you create your own, you must pass it to NvgContext when you create it.
     /// </summary>
     public interface IFontManager
     {
-        // fontmanager implementation may not support all FontGlyphBaking methods
-        // you can add here some fallback logic if your implementation doesn't support requested method
-        // note: the default implementation supports all methods
-
         /// <summary>
-        /// GetGlyphBaking.
+        /// Font manager implementation may not support all GlyphBaking methods.
+        /// You can add here some fallback logic in this case.
+        /// Note: NanoUI's built-in font managers support all glyph baking methods.
         /// </summary>
         GlyphBaking GetGlyphBaking(GlyphBaking requestedBaking);
 
-        // note: fontindex is TTC font collection index, in TTF files it is 0
-
         /// <summary>
-        /// Load.
+        /// Loads font from specified data.
+        /// Note: fontCollectionIndex is TTC font collection index, in TTF files it is 0.
         /// </summary>
         bool Load(int fontId, ReadOnlySpan<byte> data, int fontCollectionIndex);
 
         /// <summary>
-        /// GetFontVMetrics.
+        /// Gets font's vertical metrics.
         /// </summary>
         void GetFontVMetrics(int fontId, out int ascent, out int descent, out int lineGap);
 
-        // this is a scale to convert font file's internal units to pixels
-
         /// <summary>
-        /// GetPixelScale.
+        /// Scale to convert font's internal units to pixels.
         /// </summary>
         float GetPixelScale(int fontId, float size);
 
-        // returns 0, if not found (could be -1)
-
         /// <summary>
-        /// GetFontGlyphIndex.
+        /// Gets glyph index for specified codepoint.
+        /// Returns 0, if codepoint is not found.
         /// </summary>
         int GetFontGlyphIndex(int fontId, uint codepoint);
 
         /// <summary>
-        /// GetGlyphKernAdvance.
+        /// Gets kerning advance.
         /// </summary>
         int GetGlyphKernAdvance(int fontId, int glyph1, int glyph2);
 
         /// <summary>
-        /// BuildGlyphBitmap.
+        /// Builds glyph bitmap.
         /// </summary>
         bool BuildGlyphBitmap(int fontId, int glyph, float size, float scale, out int advance, out int lsb, out int x0, out int y0, out int x1, out int y1);
 
         /// <summary>
-        /// RasterizeGlyphBitmap.
+        /// Rasterizes glyph bitmap.
         /// </summary>
         void RasterizeGlyphBitmap(int fontId, Span<byte> texData, int startIndex, int outWidth, int outHeight, int outStride, float scaleX, float scaleY, int glyph, GlyphBaking fontBaking);
 
-        // NEW Shapes
-        // note: this is only called if we use for glyphs shapes (not bitmap & atlas)
-
         /// <summary>
-        /// GetGlyphShape.
+        /// Gets glyph's shape (vectorized glyph).
+        /// You can do any kind of transforms (scale etc) with these and glyphs are always sharp.
+        /// Also you have opportunity to paint glyph shape with different paint methods
+        /// (solid, gradients, image etc).
+        /// Note: this is only called, if font is initialized with GlyphBaking.Shapes.
         /// </summary>
         bool GetGlyphShape(int fontId, int fontGlyphIndex, out ReadOnlySpan<GlyphShapeCommand> shape);
 
-        // when trying to add glyph(s) & no room left in atlas. so user must expand atlas
-
         /// <summary>
-        /// OnAtlasFull.
+        /// When NanoUI tries to add glyph's bitmap to font atlas texture and
+        /// there is no room left, this method is called and you must must expand atlas texture.
         /// </summary>
         void OnAtlasFull(NvgContext ctx, int atlasWidth, int atlasHeight);
 
         /// <summary>
-        /// Dispose.
+        /// Dispose any unmanaged resources.
         /// </summary>
         void Dispose();   
     }
